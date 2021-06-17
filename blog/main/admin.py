1cd23372a0fca9994cf044256aa35e2864a50463
Denis Panin <denydies@ukr.net>
2021-06-17 16:17:36 +0300

    DZ-20

--- blog/main/admin.py
+++ blog/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import Author, Post, Subscriber, Comment, ContactUs, Book, Category
+from account.models import User
 
 """admin"""
 
@@ -11,3 +12,4 @@ admin.site.register(Category)
 admin.site.register(Subscriber)
 admin.site.register(Comment)
 admin.site.register(ContactUs)
+admin.site.register(User)
