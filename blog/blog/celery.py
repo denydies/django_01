170dd9cc23dcc166553e5cdac8062ae538f5aad7
Denis Panin <denydies@ukr.net>
2021-06-30 23:13:24 +0300

    Revert "Revert "Revert "Revert "Revert "DZ 23 docker"""""
    This reverts commit fdc1e95df64a14aa53cdada009dfc2473fee404d.

--- blog/blog/celery.py
+++ blog/blog/celery.py
@@ -2,8 +2,8 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
-app = Celery('core')
+app = Celery('blog')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
