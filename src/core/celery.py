fd6462447b726279f050658df8944d7443352303
Denis Panin <denydies@ukr.net>
2021-06-30 17:39:49 +0300

    DZ 23 docker

--- src/core/celery.py
+++ src/core/celery.py
@@ -2,8 +2,8 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-app = Celery('blog')
+app = Celery('core')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
